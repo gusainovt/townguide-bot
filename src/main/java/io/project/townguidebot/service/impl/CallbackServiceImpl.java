970a13bb90f4b2348a22bc5bb23f49f4b7ec8280
gusainovt <timurka24478@gmail.com>
2024-07-21 22:00:30 +0300

    refactor: registration buttons

--- src/main/java/io/project/townguidebot/service/impl/CallbackServiceImpl.java
+++ src/main/java/io/project/townguidebot/service/impl/CallbackServiceImpl.java
@@ -1,6 +1,6 @@
 package io.project.townguidebot.service.impl;
 
-import io.project.townguidebot.model.dto.PlaceDto;
+import io.project.townguidebot.model.ButtonCallback;
 import io.project.townguidebot.service.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,19 +8,19 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.io.IOException;
 
-import static io.project.townguidebot.service.constants.Buttons.*;
-import static io.project.townguidebot.service.constants.LogText.METHOD_CALLED;
+import static io.project.townguidebot.model.LanguageCode.RU;
 import static io.project.townguidebot.service.constants.TelegramText.REGISTER_CANCEL;
 import static io.project.townguidebot.service.constants.TelegramText.REGISTER_CONFIRMATION;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class CallbackServiceImpl implements io.project.townguidebot.service.CallbackService {
+public class CallbackServiceImpl implements CallbackService {
 
     private final SendingService sendingService;
     private final UserService userService;
@@ -32,18 +32,25 @@ public class CallbackServiceImpl implements io.project.townguidebot.service.Call
 
     /**
      * Ответ на кнопки регистрации
-     * @param update объект {@link Update} из библиотеки телеграмма
+     *
+     * @param update {@link Update} из библиотеки телеграмма
      */
     @Override
-    public EditMessageText buttonRegister(Update update, String callbackData) {
-        log.info(METHOD_CALLED + Thread.currentThread().getStackTrace()[2].getMethodName());
-        long messageId = update.getCallbackQuery().getMessage().getMessageId();
-        long chatId = update.getCallbackQuery().getMessage().getChatId();
-        if (callbackData.equals(YES_CALLBACK)) {
-            userService.registeredUser(update.getCallbackQuery().getMessage());
-            return sendingService.sendEditMessageText(REGISTER_CONFIRMATION, chatId, messageId);
-        } else {
-            return sendingService.sendEditMessageText(REGISTER_CANCEL, chatId, messageId);
+    public EditMessageText buttonRegister(Update update) {
+        ButtonCallback callback = ButtonCallback.valueOf(update.getCallbackQuery().getData());
+        Message message = update.getCallbackQuery().getMessage();
+        long messageId = message.getMessageId();
+        long chatId = message.getChatId();
+
+        log.info("Request for button registered for chat: {} and callback: {}", chatId, callback);
+        switch (callback) {
+            case RLC_RU:
+                userService.registeredUser(message, RU);
+                return sendingService.sendEditMessageText(REGISTER_CONFIRMATION, chatId, messageId);
+            case RCC_CANCEL:
+                return sendingService.sendEditMessageText(REGISTER_CANCEL, chatId, messageId);
+            default:
+                return new EditMessageText();
         }
     }
 
@@ -55,40 +62,37 @@ public class CallbackServiceImpl implements io.project.townguidebot.service.Call
      */
     @Override
     public SendMessage buttonStart(Update update, String callbackData) {
-        log.info(METHOD_CALLED + Thread.currentThread().getStackTrace()[2].getMethodName());
-        long chatId = update.getCallbackQuery().getMessage().getChatId();
-        switch (callbackData) {
-            case STORY_CALLBACK:
-                return sendingService.sendMessage(chatId, storyService.getRandomStory().getBody());
-            case PLACE_CALLBACK:
-                PlaceDto randomPlace = placeService.getRandomStory();
-                SendMessage message = sendingService.sendMessage(chatId,
-                        randomPlace.getName() +
-                                "\n" + randomPlace.getDescription());
-                return menuService.placeMenu(message);
-        }
-        return new SendMessage();
+//        log.info(METHOD_CALLED + Thread.currentThread().getStackTrace()[2].getMethodName());
+//        long chatId = update.getCallbackQuery().getMessage().getChatId();
+//        switch (callbackData) {
+//            case STORY_CALLBACK:
+//                return sendingService.sendMessage(chatId, storyService.getRandomStory().getBody());
+//            case PLACE_CALLBACK:
+//                PlaceDto randomPlace = placeService.getRandomStory();
+//                SendMessage message = sendingService.sendMessage(chatId,
+//                        randomPlace.getName() +
+//                                "\n" + randomPlace.getDescription());
+//                return menuService.placeMenu(message);
+//        }
+       return new SendMessage();
     }
 
     /**
      * Ответ на кнопки меню места
-     * @param update бъект {@link Update} из библиотеки телеграмма
+     * @param update {@link Update} из библиотеки телеграмма
      * @param callbackData Ответ от кнопки
      * @return {@link SendPhoto}
      */
     @Override
     public SendPhoto buttonPlace(Update update, String callbackData) throws IOException {
-        log.info(METHOD_CALLED + Thread.currentThread().getStackTrace()[2].getMethodName());
-        long chatId = update.getCallbackQuery().getMessage().getChatId();
-        if (callbackData.equals(PHOTO_CALLBACK)) {
-            return sendingService.sendPhoto(chatId, photoService.getPhotoPathById(14L));
-        } else {
-            return new SendPhoto();
-        }
+//        log.info(METHOD_CALLED + Thread.currentThread().getStackTrace()[2].getMethodName());
+//        long chatId = update.getCallbackQuery().getMessage().getChatId();
+//        if (callbackData.equals(PHOTO_CALLBACK)) {
+//            return sendingService.sendPhoto(chatId, photoService.getPhotoPathById(14L));
+//        } else {
+//            return new SendPhoto();
+//        }
+      return new SendPhoto();
     }
-
-
-
-
 
 }
