597fea7cebf01f9d1b741d8fbcb010165b5b8d33
gusainovt <timurka24478@gmail.com>
2024-08-26 11:27:29 +0300

    delete folder media and constants buttons

--- src/main/java/io/project/townguidebot/service/constants/Buttons.java
+++
@@ -1,7 +0,0 @@
-package io.project.townguidebot.service.constants;
-
-public class Buttons {
-//    public static final String START_STORY_BUTTON = "История";
-//    public static final String PLACE_PHOTO_BUTTON = "Фотография";
-//    public static final String START_PLACE_BUTTON = "Интересные места";
-}
