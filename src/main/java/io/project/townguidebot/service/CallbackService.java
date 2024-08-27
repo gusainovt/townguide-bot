00dfc1eb9c592cbfacc230646aeb2407f0cc97f5
gusainovt <timurka24478@gmail.com>
2024-08-27 11:20:13 +0300

    delete: registrations logic

--- src/main/java/io/project/townguidebot/service/CallbackService.java
+++ src/main/java/io/project/townguidebot/service/CallbackService.java
@@ -2,12 +2,10 @@ package io.project.townguidebot.service;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public interface CallbackService {
     SendMessage buttonStart(Update update);
-    EditMessageText buttonRegister(Update update);
     SendPhoto buttonPlace(Update update);
 
 }
--- src/main/java/io/project/townguidebot/service/impl/CallbackServiceImpl.java
+++ src/main/java/io/project/townguidebot/service/impl/CallbackServiceImpl.java
@@ -1,16 +1,16 @@
 package io.project.townguidebot.service.impl;
 
 import io.project.townguidebot.model.ButtonCallback;
-import io.project.townguidebot.service.*;
+import io.project.townguidebot.service.CallbackService;
+import io.project.townguidebot.service.PhotoService;
+import io.project.townguidebot.service.SendingService;
 import io.project.townguidebot.service.strategy.CallbackSendMessageStrategy;
-import io.project.townguidebot.service.strategy.RegisterStrategy;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -29,36 +29,17 @@ public class CallbackServiceImpl implements CallbackService {
     private final SendingService sendingService;
     private final PhotoService photoService;
 
-    private final List<RegisterStrategy> registerStrategyList;
-    private Map<ButtonCallback, RegisterStrategy> registerStrategies;
-
     private final List<CallbackSendMessageStrategy> callbackSendMessageStrategyList;
     private Map<ButtonCallback, CallbackSendMessageStrategy> callbackSendMessageStrategies;
 
 
     @PostConstruct
     public void init() {
-        registerStrategies = registerStrategyList.stream()
-                .collect(Collectors.toMap(RegisterStrategy::getButtonCallback, s -> s));
 
         callbackSendMessageStrategies = callbackSendMessageStrategyList.stream()
                 .collect(Collectors.toMap(CallbackSendMessageStrategy::getButtonCallback, s -> s));
     }
 
-    /**
-     * Ответ на кнопки регистрации
-     *
-     * @param update {@link Update} из библиотеки телеграмма
-     */
-    @Override
-    public EditMessageText buttonRegister(Update update) {
-        ButtonCallback callback = ButtonCallback.valueOf(update.getCallbackQuery().getData());
-        Message message = update.getCallbackQuery().getMessage();
-
-        log.info("Activate buttons registered for chat: {} and callback: {}", message.getChatId(), callback);
-        RegisterStrategy registerStrategy = registerStrategies.get(callback);
-        return registerStrategy.handle(message);
-    }
 
     /**
      * Ответ на кнопки меню старт
