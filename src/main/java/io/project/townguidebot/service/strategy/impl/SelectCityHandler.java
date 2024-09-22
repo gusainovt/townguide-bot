bffe32c43d243f47b7876b20c800316c12c3af6b
Timur <145484686+gusainovt@users.noreply.github.com>
2024-09-22 12:49:26 +0300

    add new button for select city

---
+++ src/main/java/io/project/townguidebot/service/strategy/impl/SelectCityHandler.java
@@ -0,0 +1,27 @@
+package io.project.townguidebot.service.strategy.impl;
+
+import io.project.townguidebot.model.ButtonCallback;
+import io.project.townguidebot.service.SendingService;
+import io.project.townguidebot.service.strategy.CallbackSendMessageStrategy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+
+@Component
+@RequiredArgsConstructor
+public class SelectCityHandler implements CallbackSendMessageStrategy {
+
+    private final ButtonCallback buttonCallback = ButtonCallback.SELECT_CITY;
+    private final SendingService sendingService;
+
+    @Override
+    public ButtonCallback getButtonCallback() {
+        return buttonCallback;
+    }
+
+    @Override
+    public SendMessage handle(Message message) {
+        return sendingService.selectCityCommandReceived(message.getChatId());
+    }
+}
