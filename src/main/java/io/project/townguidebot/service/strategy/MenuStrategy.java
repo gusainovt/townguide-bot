6f56fbe33fd91c5c04f8ee333a2a02ae9f29ac6b
gusainovt <timurka24478@gmail.com>
2025-07-22 16:40:49 +0300

    refactor: add strategy for menu

---
+++ src/main/java/io/project/townguidebot/service/strategy/MenuStrategy.java
@@ -0,0 +1,11 @@
+package io.project.townguidebot.service.strategy;
+
+import io.project.townguidebot.model.MenuType;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public interface MenuStrategy {
+    MenuType getMenuType();
+    void handle(TelegramLongPollingBot bot, Update update) throws TelegramApiException;
+}
