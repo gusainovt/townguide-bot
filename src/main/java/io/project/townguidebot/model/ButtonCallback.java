098d26bd01196106b5469e2d853f901f492ffbc3
gusainovt <timurka24478@gmail.com>
2025-07-22 16:38:53 +0300

    refactor: add annotation for type menu

--- src/main/java/io/project/townguidebot/model/ButtonCallback.java
+++ src/main/java/io/project/townguidebot/model/ButtonCallback.java
@@ -1,11 +1,16 @@
 package io.project.townguidebot.model;
 
+import io.project.townguidebot.annotation.Menu;
+
 public enum ButtonCallback {
-    RC_NO,
-    RC_YES,
-    RLC_RU,
-    RCC_CANCEL,
-    SC_STORY,
-    PC_PHOTO,
-    SC_PLACE
+    @Menu(MenuType.REG)
+    LANGUAGE_CODE_RU,
+    @Menu(MenuType.REG)
+    CANCEL,
+    @Menu(MenuType.START)
+    STORY,
+    @Menu(MenuType.PLACE)
+    PHOTO,
+    @Menu(MenuType.START)
+    PLACE
 }
