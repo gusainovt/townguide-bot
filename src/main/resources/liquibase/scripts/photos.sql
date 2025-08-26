beaaef555790e09145611f78eaba30ddf4ae2149
gusainovt <timurka24478@gmail.com>
2025-08-26 11:29:06 +0300

    refactor: new entity for Photo and new migrations

--- src/main/resources/liquibase/scripts/photos.sql
+++ src/main/resources/liquibase/scripts/photos.sql
@@ -3,12 +3,12 @@
 -- changeset gusainovt:1
 create table photos
 (
-    id        serial primary key,
-    file_path varchar(255) not null,
-    file_size bigint       not null,
+    id         serial primary key,
+    url        varchar(500) not null,
+    public_id  varchar(255) not null,
+    file_size  bigint       not null,
     media_type varchar(50),
-    data      oid,
-    place_id  bigint
+    place_id   bigint
 );
 
 -- changeset gusainovt:2
