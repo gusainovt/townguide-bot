271571f2cb967cc08fdb18a802d3c4350e41212d
Timur <145484686+gusainovt@users.noreply.github.com>
2025-09-22 12:03:04 +0300

    add buttons for story message and fix weather message

--- src/main/java/io/project/townguidebot/repository/CityRepository.java
+++ src/main/java/io/project/townguidebot/repository/CityRepository.java
@@ -3,6 +3,7 @@ package io.project.townguidebot.repository;
 import io.project.townguidebot.model.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +12,8 @@ import java.util.Optional;
 public interface CityRepository extends JpaRepository<City,Long> {
 
     @Query("SELECT c.description FROM City c WHERE c.nameEng = :nameEng")
-    Optional<String> findDescriptionByNameEng(String nameEng);
+    Optional<String> findDescriptionByNameEng(@Param("nameEng")String nameEng);
+
+    @Query("select c.name from City c where c.nameEng = :nameEng")
+    Optional<String> findCityNameByNameEng(@Param("nameEng") String nameEng);
 }
